f55491ae494de25b7d01737735e356894c415fa1
Yuuki Zachary Ishimaru <yzi170030@cslinux1.utdallas.edu>
2018-12-14 14:27:00 -0600

    Got pi.sh to use monte carlo and calculate pi

--- pi.sh
+++ pi.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-
-echo `awk -v seed=$RANDOM 'BEGIN{srand(seed);print rand()}'`
+goodVals=0;
+trials=$(($1+20));
+for i in `seq 1 $trials`;
+do
+	rand1=`awk -v seed=$RANDOM 'BEGIN{srand(seed);print rand()}'`;
+	rand2=`awk -v seed=$RANDOM 'BEGIN{srand(seed);print rand()}'`;
+	if (( `echo "sqrt( $rand1*$rand1 + $rand2*$rand2) <= 1"|bc -l` ));
+	then
+		goodVals=$((goodVals + 1));
+	fi
+done
+echo `echo "4*$goodVals / $trials"|bc -l`
