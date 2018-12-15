ceb523c28358bfe0cebc87c0fa0e1202212fb851
Yuuki Zachary Ishimaru <yzi170030@cslinux1.utdallas.edu>
2018-12-14 20:25:32 -0600

    began working on question 2, imported relevant classes

---
+++ hand.c
@@ -0,0 +1,52 @@
+#include <gsl/gsl_rng.h>
+#include <stdio.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+static int initTool();
+static void finalizeTool();
+int getRDM();
+
+gsl_rng_type *_gsl_rng_type;
+gsl_rng *_gsl_rng;
+
+int initTool(){
+	_gsl_rng = gsl_rng_alloc(gsl_rng_mt19937);
+	gsl_rng_set(_gsl_rng,(long)getpid());
+	if(_gsl_rng == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+void finalizeTool(){
+	gsl_rng_free(_gsl_rng);
+	return;
+}
+
+int getRDM(){
+	return(100*gsl_rng_uniform(_gsl_rng));
+}
+int main(int argc,char **argv ){
+ int i;
+ int option=0;
+ int percent = 0;
+ while((option=getopt(argc,argv,"p:"))!=-1){
+  switch(option){
+   case 'p':
+    percent = atoi(optarg);
+    break;
+   default:
+     fprintf(stderr,"Invalid Syntax",atoi(optarg));
+     return 1;
+    }
+  }
+ assert(initTool()==0);
+ if(getRDM()<=percent)
+  printf("Success");
+ else
+  printf("Failure");
+ finalizeTool();
+ exit(0);
+}
--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 makePi: pi.c 
 	gcc $^ -o pi -lgsl -L$$HOME/local/gsl-2.5/lib -lgslcblas -I$$HOME/local/gsl-2.5/include
+makeDealer: dealer.c
+	gcc $^ -o dealer -lgsl -L$$HOME/local/gsl-2.5/lib -lgslcblas -I$$HOME/local/gsl-2.5/include
+makeHand: hand.c
+	gcc $^ -o Hand -lgsl -L$$HOME/local/gsl-2.5/lib -lgslcblas -I$$HOME/local/gsl-2.5/include
